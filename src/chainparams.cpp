bc6b4be631e43d1643ca3a5bce3dcfb9d39d390e
cekongsb <806138244@qq.com>
2019-05-14 11:11:13 +0800

    666

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -445,7 +445,7 @@ public:
         m_assumed_chain_state_size = 1;
 
         //genesis = CreateGenesisBlockMainNet(1500296400, 31429, 0x1f00ffff); // 2017-07-17 13:00:00
-        genesis = CreateGenesisBlockMainNet(1557730800, 66557,  0x1f00ffff); // 2019-05-1 11:00:00
+        genesis = CreateGenesisBlockMainNet(1557730800, 17314,  0x1f00ffff); // 2019-05-1 11:00:00
         consensus.hashGenesisBlock = genesis.GetHash();
         
         bool fNegative;
